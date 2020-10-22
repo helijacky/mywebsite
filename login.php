36af336cd5acebc3fce2b42b0f922ea1dc084b6e
helijacky <helijackybu@gmail.com>
2020-10-22 07:59:20 +0700

    Add files via upload

---
+++ login.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+   <link href=".\CSS\stylelog.css" rel="stylesheet">
+   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+ <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+    <title>Login</title>
+
+
+
+
+  </head>
+
+
+
+  <body>
+
+
+    <div class="container">
+    	<div class="d-flex justify-content-center h-100">
+    		<div class="card">
+    			<div class="card-header">
+            <a style="color:white;font-size:40px" href="index.php" >ATN store</a>
+    				<h4 style="color:white">Login</h4>
+    				<div class="d-flex justify-content-end social_icon">
+    					<span><i class="fab fa-facebook-square"></i></span>
+    					<span><i class="fab fa-youtube-square"></i></span>
+
+    				</div>
+    			</div>
+    			<div class="card-body">
+    				<form>
+    					<div class="input-group form-group">
+    						<div class="input-group-prepend">
+    							<span class="input-group-text"><i class="fas fa-user"></i></span>
+    						</div>
+    						<input type="text" class="form-control" placeholder="username">
+
+    					</div>
+    					<div class="input-group form-group">
+    						<div class="input-group-prepend">
+    							<span class="input-group-text"><i class="fas fa-key"></i></span>
+    						</div>
+    						<input type="password" class="form-control" placeholder="password">
+    					</div>
+    					<div class="row align-items-center remember">
+    						<input type="checkbox">Remember Me
+    					</div>
+    					<div class="form-group">
+    						<input type="submit" value="Login" class="btn btn-outline-warning float-right login_btn ">
+    					</div>
+    				</form>
+    			</div>
+    			<div class="card-footer">
+    				<div class="d-flex justify-content-center links">
+    					Don't have an account?<a href="signup.php">Sign Up</a>
+    				</div>
+    				<div class="d-flex justify-content-center">
+    					<a href="#">Forgot your password?</a>
+    				</div>
+    			</div>
+    		</div>
+    	</div>
+    </div>
+
+
+
+  </body>
+</html>
